4bb48f3105b83a6db9173d5c84a58f8f26669f2b
beckitrue <3496219+beckitrue@users.noreply.github.com>
2024-04-20 17:07:44 -0500

    additional steps from the course

--- foundations/slices/slices.go
+++ foundations/slices/slices.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -25,4 +26,69 @@ func main() {
 	fmt.Printf("s2: len=%d, cap=%d\n", len(s2), cap(s2))
 	fmt.Printf("s3: len=%d, cap=%d\n", len(s3), cap(s3))
 
+	s4 := appendInt(s3, 200)
+	fmt.Printf("s4 = %#v\n", s4)
+
+	s2 = []int{10, 20, 30, 40, 50, 60, 70}
+
+	s5 := appendInt(s2, 1000)
+	fmt.Printf("s5 = %#v\n", s5)
+	fmt.Printf("s5: len=%d, cap=%d\n", len(s5), cap(s5))
+
+	fmt.Println(concat([]string{"A", "B"}, []string{"C", "D", "E"})) // [A B C D E]
+
+	vs := []float64{2, 1, 3}
+	fmt.Println(median(vs))
+
+	vs = []float64{2, 1, 4, 3}
+	fmt.Println(median(vs))
+
+	fmt.Println(median(nil))
+
+}
+
+func median(values []float64) (float64, error) {
+	// calculate the median of a slice of float64 values
+	// values is the slice of float64 values
+	// returns the median value
+	// check for empty slice
+	if len(values) == 0 {
+		return 0, fmt.Errorf("empty slice")
+	}
+	s := make([]float64, len(values))
+	copy(s, values)
+	sort.Float64s(s)
+	mid := len(s) / 2
+	if mid%2 == 1 {
+		return s[mid], nil
+	}
+	v := (s[mid-1] + s[mid]) / 2
+	return v, nil
+}
+
+func concat(s1 []string, s2 []string) []string {
+	// concatenate two slices
+	// s1 and s2 are the slices to concatenate
+	s := make([]string, len(s1)+len(s2))
+	copy(s, s1)
+	copy(s[len(s1):], s2)
+	return s
+}
+
+func appendInt(s []int, v int) []int {
+	// append an integer to the end of a slice
+	// s is the slice and v is the value to append
+	i := len(s)
+
+	if len(s) < cap(s) {
+		s = s[:len(s)+1]
+	} else {
+		fmt.Println("slice is full, need to allocate a new slice")
+		newSlice := make([]int, 2*len(s)+1)
+		copy(newSlice, s)
+		s = newSlice
+	}
+
+	s[i] = v
+	return s
 }
